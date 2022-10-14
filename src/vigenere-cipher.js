ec670f49d9d4d4821175d6192caea2f59100c6a9
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-10-14 17:23:11 +0300

    solve character count task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || message === null || key === undefined || key === null) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    let res = '';
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    if(this.isDirect === true || this.isDirect === undefined) {
+      res = key.padEnd(message.length, key);
+      console.log(res)
+    } else {
+      return message.split('').reverse().join('');
+    }
+    
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || encryptedMessage === null || key === undefined || key === null) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    let res = '';
+
+    if(this.isDirect === true || this.isDirect === undefined) {
+
+    } else {
+      return message.split('').reverse().join('');
+    }
+    
   }
 }
 
