de024bda4a2ffec9ca42fbdc6324a3e71774a792
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-10-16 18:47:08 +0300

    feat: solve vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -25,35 +25,119 @@ class VigenereCipheringMachine {
   }
 
   encrypt(message, key) {
-    if (message === undefined || message === null || key === undefined || key === null) {
-      throw new Error("Incorrect arguments!");
-    }
+    if (!message || !key) throw new Error("Incorrect arguments!");
 
-    let res = '';
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let res = [];
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    if(this.isDirect === true || this.isDirect === undefined) {
-      res = key.padEnd(message.length, key);
-      console.log(res)
-    } else {
-      return message.split('').reverse().join('');
-    }
-    
+    message = message.toString().toUpperCase();
+    let cleanedMessage = (message.replace(/\W/gi, '').replace(/\d/gi, '')).toString().toUpperCase();
+    key = key.toString().toUpperCase();
+    let messageValues = message.split(''); 
+    key = key.padEnd(cleanedMessage.length, key); 
+    let keyValues = key.split(''); 
+
+      for (let i = 0; i < messageValues.length; i++) {
+        for (let a = 0; a < alphabet.length; a++) {
+          if(alphabet[a] === messageValues[i]) {
+            messageValues.splice(i, 1, alphabet.indexOf(alphabet[a]))
+          }
+        }
+      }
+
+      for (let i = 0; i < keyValues.length; i++) {
+        for (let a = 0; a < alphabet.length; a++) {
+          if(alphabet[a] === keyValues[i]) {
+            keyValues.splice(i, 1, alphabet.indexOf(alphabet[a]))
+          }
+        }
+      }
+
+       let a = 0;
+        for (let i = 0; i < messageValues.length; i++) {
+            if(typeof(messageValues[i]) === 'number') {
+             res.push((messageValues[i] + keyValues[a]) % 26);
+             a++
+           } else {
+             res.push(messageValues[i])
+           }
+         }
+
+        for (let i = 0; i < res.length; i++) {
+           for (let a = 0; a < alphabet.length; a++) {
+             if(alphabet.indexOf(alphabet[a]) === res[i]) {
+               res.splice(i, 1, alphabet[a])
+             }
+           }
+        }
+
+
+       res = res.join('');
+
+       if(this.isDirect === true || this.isDirect === undefined) {
+         return res
+       } else {
+         return res.split('').reverse().join('');
+       }
   }
 
   decrypt(encryptedMessage, key) {
-    if (encryptedMessage === undefined || encryptedMessage === null || key === undefined || key === null) {
-      throw new Error("Incorrect arguments!");
+    if (!encryptedMessage || !key) throw new Error("Incorrect arguments!");
+
+     let res = [];
+     const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    encryptedMessage = encryptedMessage.toString().toUpperCase();
+     let cleanedMessage = (encryptedMessage.replace(/\W/gi, '').replace(/\d/gi, '')).toString().toUpperCase();
+     key = key.toString().toUpperCase();
+     let messageValues = encryptedMessage.split('');
+     key = key.padEnd(cleanedMessage.length, key);
+     let keyValues = key.split('');
+      
+     for (let i = 0; i < messageValues.length; i++) {
+      for (let a = 0; a < alphabet.length; a++) {
+        if(alphabet[a] === messageValues[i]) {
+          messageValues.splice(i, 1, alphabet.indexOf(alphabet[a]))
+        }
+      }
     }
 
-    let res = '';
-
-    if(this.isDirect === true || this.isDirect === undefined) {
-
-    } else {
-      return message.split('').reverse().join('');
+    for (let i = 0; i < keyValues.length; i++) {
+      for (let a = 0; a < alphabet.length; a++) {
+        if(alphabet[a] === keyValues[i]) {
+          keyValues.splice(i, 1, alphabet.indexOf(alphabet[a]))
+        }
+      }
     }
-    
+
+     let a = 0;
+      for (let i = 0; i < messageValues.length; i++) {
+          if(typeof(messageValues[i]) === 'number') {
+           let f = messageValues[i] - keyValues[a];
+           if(f < 0) {
+            res.push(f + 26);
+           } else res.push(f % 26)
+           a++
+         } else {
+           res.push(messageValues[i])
+         }
+       }
+
+      for (let i = 0; i < res.length; i++) {
+         for (let a = 0; a < alphabet.length; a++) {
+           if(alphabet.indexOf(alphabet[a]) === res[i]) {
+             res.splice(i, 1, alphabet[a])
+           }
+         }
+      }
+
+      res = res.join('');
+
+      if(this.isDirect === true || this.isDirect === undefined) {
+         return res
+       } else {
+         return res.split('').reverse().join('');
+       }
   }
 }
 
