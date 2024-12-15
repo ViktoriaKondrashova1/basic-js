235573b09e2671ed1b720b8d3ed286b53cec6f0d
Viktoria Kondrashova <kavidmi@gmail.com>
2024-12-15 18:03:30 +0300

    feat: solve getCommonCharacterCount and dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,13 +6,13 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
@@ -20,15 +20,20 @@ const HALF_LIFE_PERIOD = 5730;
 function dateSample(sampleActivity) {
   const MODERN_ACTIVITY = 15;
   const HALF_LIFE_PERIOD = 5730;
-  let res;
 
-  if((typeof(sampleActivity) !== 'string') || (isNaN(sampleActivity)) || (sampleActivity.length = 0) || (sampleActivity <= 0) || (sampleActivity > MODERN_ACTIVITY)) {
-    return false
-  } else {
-    return res = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / ((0.693 / HALF_LIFE_PERIOD)));
-  }  
+  if (
+    typeof sampleActivity !== "string" ||
+    isNaN(+sampleActivity) ||
+    arguments.length === 0 ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY
+  )
+    return false;
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+  );
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -12,21 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  s1 = s1.split('').sort();
-  s2 = s2.split('').sort();
-  let count = 0;
-
-  for (let i = 0; i < s1.length; i++) {
-    for (let a = 0; a < s2.length; a++) {
-      if(s1[i] === s2[a]) {
-        count++
-        i++
-      }
+  const arr1 = s1.split("");
+  const arr2 = s2.split("");
+  let counter = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      const idx = arr2.indexOf(arr1[i]);
+      arr2.splice(idx, 1);
+      counter++;
     }
   }
-  return count
+  return counter;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
